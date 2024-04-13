c859261d41fdde7cd305286127c02001d6920310
Ernani Da Silva Neto <ernda-si@c2r10s1.42lisboa.com>
2024-04-13 19:36:29 +0100

    Update changed strlcat and added a file called .vscode

--- strlcat.c
+++ strlcat.c
@@ -6,32 +6,29 @@
 /*   By: ernda-si <ernda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 17:23:29 by ernda-si          #+#    #+#             */
-/*   Updated: 2024/04/13 18:00:11 by ernda-si         ###   ########.fr       */
+/*   Updated: 2024/04/13 19:33:31 by ernda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-int	ft_strlen(char *str)
-{
-	int i;
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
 size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
-	if (*dst++)
-	{
-		
-	}
+	size_t	i;
+	size_t	i2;
+
+	i = 0;
+	i2 = 0;
+	while (dst[i] != '\0' && size > i)
+		i++;
+	while (src[i2] != '\0')
+		i2++;
+	return (i + i2);
 }
 
 int	main(void)
 {
-	printf("Original strlcat: %i\n", strlcat("Abacaxi", "Uva", sizeof(char) * 0));
-	printf("My strlcat: %i\n", ft_strlcat("Abacaxi", "Uva", sizeof (char) * 0));
+	printf("Original strlcat: %ld\n", strlcat("Abacaxi", "Uva", sizeof(char) * 3));
+	printf("My strlcat: %ld\n", ft_strlcat("Abacaxi", "Uva", sizeof (char) * 3));
 }
