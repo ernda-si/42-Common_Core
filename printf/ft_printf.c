2f1f9ba2e81d88b2cc48496844da2b67b3c30522
Ernani Da Silva Neto <ernda-si@c1r6s5.42lisboa.com>
2024-05-21 19:26:13 +0100

    printfupdate21/05

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ernda-si <ernda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:41:46 by ernda-si          #+#    #+#             */
-/*   Updated: 2024/05/20 16:52:18 by ernda-si         ###   ########.fr       */
+/*   Updated: 2024/05/21 18:22:59 by ernda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+char	*ft_putstr(char *str)
 {
 	int	i;
 
@@ -24,37 +24,26 @@ void	ft_putstr(char *str)
 			write (1, &str[i], 1);
 			i++;
 	}
+	return (0);
 }
 
-char	ft_putchar(char c)
+int	ft_putchar(char c)
 {
 		write(1, &c, 1);
-		return(c);
+		return (1);
 }
 
-void	ft_putnbr(int nb)
+int	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
-	{
-			ft_putstr("-2147483648");
-	}
-	else
-	{
-			if (nb < 0)
-			{
-					ft_putchar('-');
-					nb *= -1;
-			}
-			if (nb <= 9)
-			{
-					ft_putchar(nb + '0');
-			}
-			if (nb >= 10)
-			{
-					ft_putnbr(nb / 10);
-					ft_putnbr(nb % 10);
-			}
-	}
+	int	i;
+
+	i = 0;
+	if (nb > 9 || nb < -9)
+		i += ft_putnbr(nb / 10);
+	else if (nb < 0)
+		i += write (1, "-", 1);
+	i += write(1, &"0123456789"[nb % 10 * ((nb > 0) - (nb < 0))], 1);
+	return (i);
 }
 
 int	ft_strlen(char *str)
@@ -69,40 +58,60 @@ int	ft_strlen(char *str)
 
 int	ft_printf(const char *str, ...)
 {
-	va_list ptr;
-	va_start (ptr, str);
+	va_list		ptr;
+	char		token[1024] = {0};
+	int			i;
+	int			j;
+	int			count;
 
-	char	token[1000];
-	int		i;
-	int		j;
-
-	j = 0;
+	va_start	(ptr, str);
 	i = 0;
+	j = 0;
+	count = 0;
+	if (!str)
+		return (-1);
 	while (str[j])
 	{
 		token[i++] = str[j++];
+		count++;
 		if (str[j + 1] == '%' || str[i + 1] == '\0')
 		{
 			token[i] = '\0';
 			i = 0;
 			if (token[0] != '%')
-			{
 				write(1, token, ft_strlen(token));
-			}
 			else
 			{
 				while (token[j++])
-					if (token[j] == 'c')
-						ft_putchar(token[j]);
+				{
+					if (token[j] == 'c' || token[j] == 'C')
+					{
+						ft_putchar(va_arg(ptr, int));
+						count++;
+					}
+					else if (token[j] == 'd' || token[j] == 'D' || token[j] == 'i')
+						count += ft_putnbr(va_arg(ptr, int));
+					else if (token[j] == 's' || token[j] == 'S')
+						count += ft_strlen(ft_putstr(va_arg(ptr, char *)));
+					else if (token[j] == 'p')
+					{
+						write(1, va_arg(ptr, int *), 3);
+						count++;
+					}
+				}
 			}
 		}
 	}
-	return (0);
+	va_end(ptr);
+	return (count);//{sS} {p} {dDi} oO uU xX {cC}
 }
 
 int	main(void)
 {
-	int	a = 42;
-	ft_printf("My printf: %c\n", a);
-	printf("Original printf: %c\n", a);
+	// int		i = 42;
+	void	*p;
+
+	p = "a";
+	ft_printf("My printf: %p\n", p);
+	printf("Original printf: %p\n", p);
 }
