904761c134d7abd831c6c3bae0b054651d7e2c7a
Ernani Da Silva Neto <ernda-si@c1r5s5.42lisboa.com>
2024-05-23 19:05:01 +0100

    printf modified 23/05

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -5,16 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ernda-si <ernda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/20 12:41:46 by ernda-si          #+#    #+#             */
-/*   Updated: 2024/05/21 18:22:59 by ernda-si         ###   ########.fr       */
+/*   Created: 2024/05/23 13:24:59 by ernda-si          #+#    #+#             */
+/*   Updated: 2024/05/23 19:04:33 by ernda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-char	*ft_putstr(char *str)
+static int	ft_putstr(char *str)
 {
 	int	i;
 
@@ -24,16 +25,16 @@ char	*ft_putstr(char *str)
 			write (1, &str[i], 1);
 			i++;
 	}
-	return (0);
+	return (i);
 }
 
-int	ft_putchar(char c)
+static int	ft_putchar(char c)
 {
 		write(1, &c, 1);
 		return (1);
 }
 
-int	ft_putnbr(int nb)
+static int	ft_putnbr(int nb)
 {
 	int	i;
 
@@ -46,7 +47,44 @@ int	ft_putnbr(int nb)
 	return (i);
 }
 
-int	ft_strlen(char *str)
+static int	ft_puthexasmall(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb > 9 || nb < -9)
+		i += ft_puthexasmall(nb / 16);
+	else if (nb < 0)
+		i += write (1, "-", 1);
+	i += write(1, &"0123456789abcdef"[nb % 16 * ((nb > 0) - (nb < 0))], 1);
+	return (i);
+}
+
+static int	ft_puthexabig(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb > 9 || nb < -9)
+		i += ft_puthexabig(nb / 16);
+	else if (nb < 0)
+		i += write (1, "-", 1);
+	i += write(1, &"0123456789ABCDEF"[nb % 16 * ((nb > 0) - (nb < 0))], 1);
+	return (i);
+}
+
+static int	ft_putunbr(unsigned int unb)
+{
+	int	i;
+
+	i = 0;
+	if (unb > 9)
+		i += ft_putunbr(unb / 10);
+	i += write(1, &"0123456789"[unb % 10], 1);
+	return (i);
+}
+
+static int	ft_strlen(char *str)
 {
 	int	i;
 
@@ -58,60 +96,59 @@ int	ft_strlen(char *str)
 
 int	ft_printf(const char *str, ...)
 {
-	va_list		ptr;
-	char		token[1024] = {0};
-	int			i;
-	int			j;
-	int			count;
+	va_list args;
+	int len;
 
-	va_start	(ptr, str);
-	i = 0;
-	j = 0;
-	count = 0;
+	len = 0;
+	va_start(args, str);
 	if (!str)
 		return (-1);
-	while (str[j])
+	while (*str)
 	{
-		token[i++] = str[j++];
-		count++;
-		if (str[j + 1] == '%' || str[i + 1] == '\0')
+		if (*str != '%')
+			write(1, str, 1);
+		else
 		{
-			token[i] = '\0';
-			i = 0;
-			if (token[0] != '%')
-				write(1, token, ft_strlen(token));
-			else
-			{
-				while (token[j++])
-				{
-					if (token[j] == 'c' || token[j] == 'C')
-					{
-						ft_putchar(va_arg(ptr, int));
-						count++;
-					}
-					else if (token[j] == 'd' || token[j] == 'D' || token[j] == 'i')
-						count += ft_putnbr(va_arg(ptr, int));
-					else if (token[j] == 's' || token[j] == 'S')
-						count += ft_strlen(ft_putstr(va_arg(ptr, char *)));
-					else if (token[j] == 'p')
-					{
-						write(1, va_arg(ptr, int *), 3);
-						count++;
-					}
-				}
-			}
+			*str++;
+			// if (*str == 'd')
+			// 	len += ft_putnbr(va_arg(args, double));
+			if (*str == 'i' || *str == 'd')
+				len += ft_putnbr(va_arg(args, int));
+			else if (*str == 'c' || *str == 'C' || *str == '%')
+				len += ft_putchar(va_arg(args, int));
+			else if (*str == 's')
+				len += ft_putstr(va_arg(args, char *));
+			else if (*str == 'u')
+				len += ft_putunbr(va_arg(args, unsigned int));
+			else if (*str == 'x')
+				len += ft_puthexasmall(va_arg(args, int));
+			else if (*str == 'X')
+				len += ft_puthexabig(va_arg(args, int));
 		}
+		*str++;
+		len++;
 	}
-	va_end(ptr);
-	return (count);//{sS} {p} {dDi} oO uU xX {cC}
+	va_end(args);
+	return (len);
 }
 
 int	main(void)
 {
-	// int		i = 42;
-	void	*p;
+	int		nb;
+	char	*str;
 
-	p = "a";
-	ft_printf("My printf: %p\n", p);
-	printf("Original printf: %p\n", p);
+	str = "abacaxi";
+	nb = 42;
+	ft_printf("my printf len: %d\n", ft_printf("my printf: %d %i %c %s %u %x %X\n", nb, nb, nb, str, 2147483648, 123));
+	printf("original printf len: %d\n", printf("original printf: %d %i %c %s %u %x %X\n", nb, nb, nb, str, 2147483648, 123));
 }
+
+/*%c Prints a single character.-----
+• %s Prints a string (as defined by the common C convention).-----
+• %p The void * pointer argument has to be printed in hexadecimal format. -----/////s
+• %d Prints a decimal (base 10) number.-----
+• %i Prints an integer in base 10.-----
+• %u Prints an unsigned decimal (base 10) number.-----
+• %x Prints a number in hexadecimal (base 16) lowercase format.-----
+• %X Prints a number in hexadecimal (base 16) uppercase format.-----
+• %% Prints a percent sign.----- */
