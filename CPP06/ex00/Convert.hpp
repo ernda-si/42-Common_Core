838b0a8a50522568f38a1647c5751e98e9d8cd15
Ernani Da Silva Neto <ernda-si@c2r8s10.42lisboa.com>
2026-04-21 19:05:05 +0100

    updated cpp06

--- CPP06/ex00/Convert.hpp
+++ CPP06/ex00/Convert.hpp
@@ -6,7 +6,7 @@
 /*   By: ernda-si <ernda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 16:59:48 by ernda-si          #+#    #+#             */
-/*   Updated: 2026/04/20 19:21:56 by ernda-si         ###   ########.fr       */
+/*   Updated: 2026/04/21 19:00:41 by ernda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,53 @@
 class ScalarConvert
 {
 private:
-	
+
 	ScalarConvert();
 	~ScalarConvert();
-	//recebe como cada valor requirido e converte para string logo apos
-	static std::string to_string(unsigned char var)
+
+	static std::string to_string(unsigned char value)
 	{
-		if (!var)
+		if (!value)
 			return ("impossible");
-		return (std::string(1, var));
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
+		std::cout << "double: " << static_cast<double>(value) << std::endl;
+		std::cout << "float: " << static_cast<float>(value) << std::endl;
+		return (std::string(1, value));
 	};
-	// static std::string to_string(int var);
-	// static std::string to_string(float var);
-	// static std::string to_string(double var);
-	
-	
+
+	static std::string to_string(int value)
+	{
+		if (!value)
+			return ("impossible");
+		std::cout << "char: " << static_cast<char>(value) << std::endl;
+		std::cout << "double: " << static_cast<double>(value) << std::endl;
+		std::cout << "float: " << static_cast<float>(value) << std::endl;
+		return (std::string(1, value));
+	};
+
+	static std::string to_string(double value)
+	{
+		if (!value)
+			return ("impossible");
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
+		std::cout << "char: " << static_cast<char>(value) << std::endl;
+		std::cout << "float: " << static_cast<float>(value) << std::endl;
+		return (std::string(1, value));
+	};
+
+	static std::string to_string(float value)
+	{
+		if (!value)
+			return ("impossible");
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
+		std::cout << "double: " << static_cast<double>(value) << std::endl;
+		std::cout << "char: " << static_cast<char>(value) << std::endl;
+		return (std::string(1, value));
+	};
+
 public:
 
-	static size_t	find_n_times(std::string str, char target)
+	static size_t	countChars(std::string str, char target)
 	{
 		size_t	occur = 0;
 		int		i = -1;
@@ -46,33 +75,27 @@ public:
 			if (str[i] == target)
 				occur++;
 		}
-		std::cout << target << ": " << occur << std::endl;
 		return occur;
 	}
 
-	
 	static bool is_number(const std::string &var)
 	{
-		if (var.empty() || find_n_times(var, '-') > 1 
-			|| find_n_times(var, '.') > 1 || find_n_times(var, 'f') > 1 
+		if (var.empty() || countChars(var, '-') > 1 || countChars(var, '.') > 1 || countChars(var, 'f') > 1 
 				|| var.find_first_not_of("1234567890-.f") != std::string::npos 
 					|| (var.find('.') != std::string::npos && !isdigit(var[var.find('.') + 1])) 
-						|| (var.find('-') != std::string::npos && !isdigit(var[var.find('-') + 1])) 
+						|| (var.find('-') != std::string::npos && (!isdigit(var[var.find('-') + 1]) || var.find('-') != 0)) 
 							|| (var.find('f') != std::string::npos && var[var.find('f') + 1]))
 			return false;
-		std::cout << var << std::endl;
 		return true;
 	}
 
 	static void convert(const std::string &var) {
 
 		double	value = 0;
+
 		if (is_number(var))
-		{
 			std::cout << var << " is a number" << std::endl;
-			return ;
-		}
-		if (var == "nan" || var == "nanf")
+		else if (var == "nan" || var == "nanf")
 			value = std::numeric_limits<double>::quiet_NaN();
 		else if (var.size() == 1)
 			value = static_cast<double>(var[0]);
@@ -81,11 +104,8 @@ public:
 		else if (var == "-inf" || var == "-inff")
 			value = -std::numeric_limits<double>::infinity();
 		else
-		{
 			std::cerr << "Not a number" << std::endl;
-			return ;
-		}
-		std::cout << "char: " << to_string(static_cast<unsigned char>(value)) << std::endl;
+		std::cout << to_string(static_cast<int>(value)) << std::endl;
 	};
 };
 
